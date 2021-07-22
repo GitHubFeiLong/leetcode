ef4ddc411ff52827122c74d2c10c97d72074e06b
BitbucketFeiLong <1696741038@qq.com>
2021-07-22 17:32:46 +0800

    未完成

--- src/main/java/easy/E20.java
+++ src/main/java/easy/E20.java
@@ -1,5 +1,10 @@
 package easy;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /**
  * 20有效的括号
  * @author e-Feilong.Chen
@@ -10,5 +15,38 @@ package easy;
 public class E20 {
     public static void main(String[] args) {
 
+        Map<String, String> map = new HashMap<>(6);
+        map.put("(", ")");
+        map.put(")", "(");
+        map.put("{", "}");
+        map.put("}", "{");
+        map.put("[", "]");
+        map.put("]", "[");
+
+
+        String s = "([)]";
+        String[] split = s.split("");
+        if (split.length % 2 != 0) {
+            
+        }
+        Stack<String> stack = new Stack();
+        
+        for (int i = 0; i < split.length; i++) {
+            String s1 = split[i];
+            if (i != 0) {
+                // 添加时检查上一个元素和本次添加的元素是否可以抵消
+                String peek = stack.peek();
+                if (!s1.equals(map.get(peek))) {
+                    stack.push(s1);
+                }
+            } else {
+                stack.push(s1);
+            }
+            
+
+
+        }
+
+        System.out.println(Arrays.toString(split));
     }
 }
